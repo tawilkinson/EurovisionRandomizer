50906b5533703898f7641d6ddd0c2b89e40d70e2
Tom Wilkinson <tom@tawilkinson.com>
2019-05-07 19:20:50 +0100

    Multiple updates to signifanctly improve the Randomizer
    - correctly does duyplicates
    - doesn't hang on trying to find a specific random number due to `random.shuffle()`

--- EURsorandom.py
+++ EURsorandom.py
@@ -1,11 +1,31 @@
 import os
 import csv
 import random
-from datetime import datetime
+import copy
+from collections import defaultdict
+outdict = {}
+duplicate_dict = defaultdict(lambda:1)
 
+def randomizer(names, country_list):
+    random.shuffle(country_list)
+    num_processed = 0
+    attempts = 0
+    for name in names:
+        try:
+            key = country_list[-1]
+            if key in outdict.keys():
+                attempts += 1
+                count = duplicate_dict[key] + 1
+                duplicate_dict[key] = count
+                key += " (" + str(count) + ")"
+            country_list.pop()
+            outdict[key] = name
+            num_processed += 1
+        except IndexError:
+            pass
+    return num_processed, attempts
 
 def EURandom():
-    # now = datetime.now()
     countries = "eurovision2019.participants"
     names = "names.txt"
     clist = []
@@ -21,8 +41,7 @@ def EURandom():
         for row in creader:
             clist.append(row[0])
 
-    cdict = dict(enumerate(clist))
-    n_countries = len(cdict)
+    n_countries = len(clist)
     print("Number of Countries: {0}".format(n_countries))
 
     ninput = input("Enter names file (empty uses default): ")
@@ -37,31 +56,22 @@ def EURandom():
 
     n_people = len(nlist)
     print("Number of People: {0}".format(n_people))
-    toomany = False
 
     if n_people > n_countries:
         print("Too many people. There will be duplicates...")
         print("(Number of duplicates counted in brackets)")
-        toomany = True
 
     random.seed()
-    outdict = {}
-    nums = []
-    attempt = 0
-    for i in nlist:
-        while True:
-            num = random.randrange(1, len(cdict))
-            if num not in nums:
-                nums.append(num)
-                outdict[cdict.get(num)] = i
-                break
-            elif toomany:
-                attempt += 1
-                key = cdict.get(num) + " (" + str(attempt) + ")"
-                outdict[key] = i
-                break
-            else:
-                pass
+    processed = 0
+    total_attempts = 0
+    while True:
+        count, attempts = randomizer(nlist[:n_countries], clist.copy())
+        processed += count
+        total_attempts += attempts
+        if processed >= n_people:
+            break
+        for i in range(count):
+            nlist.pop(0)
 
     out = ""
     n = 0
@@ -69,8 +79,7 @@ def EURandom():
         n += 1
         out += (str(n) + " " + j + ": " + i + "\n")
 
-    if toomany:
-        out += "Total Duplicates: {0}\n".format(attempt)
+    out += "Total Duplicates: {0}\n".format(total_attempts)
     print(out)
 
     # out = "Year: {0}\nNumber of Countries: {1}\nNumber of People: {2}\n\n"\
